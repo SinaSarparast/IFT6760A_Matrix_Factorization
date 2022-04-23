fixed masking bug

multihead_attention.py
@@ -20,7 +20,7 @@ class ScaledDotProductAttention(nn.Module):
         attn = attn / self.temperature
 
         if mask is not None:
-            attn = attn.masked_fill(mask, -np.inf)
+            attn = attn.masked_fill(torch.gt(mask, 0), -np.inf)
 
         attn = self.softmax(attn)
         attn = self.dropout(attn)
@@ -72,7 +72,7 @@ class MultiHeadAttention(nn.Module):
         k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k) # (n*b) x lk x dk
         v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v) # (n*b) x lv x dv
 
-        attn_mask = attn_mask.repeat(n_head*attn_mask.size(0), 1, 1) # (n*b) x .. x ..
+        attn_mask = attn_mask.repeat(q.size(0), 1, 1) # (n*b) x .. x ..
         output, attn = self.attention(q, k, v, mask=attn_mask)
 
         output = output.view(n_head, sz_b, len_q, d_v)

train.py
@@ -10,9 +10,9 @@ test_data = torch.load(os.path.join(data_dir,'test_data.pt'), map_location=devic
 vocab = torch.load(os.path.join(data_dir,'vocab.pt'), map_location=device )
 
 # only use for debugging
-# train_data = torch.randint(0, 20, (100,20))
-# val_data = torch.randint(0, 20, (100,20))
-# test_data = torch.randint(0, 20, (100,20))
+train_data = torch.randint(0, 100, (150,50))
+val_data = torch.randint(0, 100, (100,50))
+test_data = torch.randint(0, 100, (100,50))
 
 
 if __name__ == "__main__":
@@ -38,13 +38,15 @@ if __name__ == "__main__":
     args.ntokens = len(vocab)  # size of vocabulary
 
     # only for debugging
-    # args.ntokens = 20
+    args.ntokens = 100
 
     # warmup_steps = 4000
     # label_smoothing 
 
     #build the model
     model = TransformerModel(args).to(device)
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('The model has {} parameters.'.format(pytorch_total_params))
 
     trainer = Trainer(model, train_data, val_data, test_data, args)
     trainer.train(args.epochs)

trainer.py
@@ -49,6 +49,10 @@ class Trainer():
         # self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, 4000.0, gamma=0.95, verbose=True)
         self.scheduler = ScheduledOptim(self.optimizer,self.lr,self.d_model,n_warmup_steps=10,n_steps=self.current_epoch)
 
+        pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        st='The model has {} parameters.\n'.format(pytorch_total_params)
+        write_to_log(self.logs_dir,'training_log.log',st)
+
         pass
 
     def step(self, epoch) -> None:
@@ -68,8 +72,8 @@ class Trainer():
         for batch, i in enumerate(range(0, self.train_data.size(0) - 1, self.bptt)):
             data, targets = get_batch(self.train_data, i, self.bptt)
             batch_size = data.size(0)
-            if batch_size != self.bptt:  # only on last batch
-                src_mask = src_mask[:batch_size, :batch_size]
+            # if batch_size != self.bptt:  # only on last batch
+            #     src_mask = src_mask[:batch_size, :batch_size]
             output = self.model(data, src_mask)
             loss = self.criterion(output.view(-1, self.ntokens), targets)
 
@@ -86,7 +90,7 @@ class Trainer():
                 ppl = math.exp(cur_loss)
                 st = f'| epoch {epoch:3d} | {batch:5d}/{self.num_batches:5d} batches | 'f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | 'f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}'
                 print(st)
-                write_to_log(self.logs_dir,'training_log.log',st)
+                write_to_log(self.logs_dir,'training_log.log',st+'\n')
                 total_loss = 0
                 start_time = time.time()
             
@@ -109,8 +113,8 @@ class Trainer():
             for i in range(0, eval_data.size(0) - 1, self.bptt):
                 data, targets = get_batch(eval_data, i, self.bptt)
                 batch_size = data.size(0)
-                if batch_size != self.bptt:
-                    src_mask = src_mask[:batch_size, :batch_size]
+                # if batch_size != self.bptt:
+                #     src_mask = src_mask[:batch_size, :batch_size]
                 output = model(data, src_mask)
                 output_flat = output.view(-1, self.ntokens)
                 total_loss += batch_size * self.criterion(output_flat, targets).item()